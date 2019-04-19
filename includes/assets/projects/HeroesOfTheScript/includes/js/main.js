ce14454b1daaa9715f1a2258349714fa4f344428
GrigorisParaskevakos <paraskevakosg@gmail.com>
2019-04-20 01:34:11 +0300

    play

--- includes/assets/projects/HeroesOfTheScript/includes/js/main.js
+++ includes/assets/projects/HeroesOfTheScript/includes/js/main.js
@@ -1,3 +1,33 @@
-function arrowFunction(){
+/**
+ * audio
+ */
+var player = function () {
+
+    //let x = $("#muse");            
+    this.musicPlayer = {
+
+        play: function () {
+            document.getElementById("muse").play();
+            document.getElementById("muse").load();
+            document.getElementById("muse").volume = 1.000000;
+            setTimeout(
+                function () {
+                    document.getElementById("muse").play();
+                }, 1);
+        },
+
+        pause: function () {
+            x.pause();
+        }
+
+    };
+};
+
+let mp3Player = new player();
+
+/**
+ * Ui btn
+ */
+function arrowFunction() {
     location.href = "../ColmarAcademy/index.html";
 }
