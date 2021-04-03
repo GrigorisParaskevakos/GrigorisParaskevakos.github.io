95efbbf50bea4fbb7c1340a25ba7c789ed2c09d0
GrigorisParaskevakos <paraskevakosg@gmail.com>
2021-04-03 20:32:34 +0300

    checks for auto drops

--- includes/assets/projects/HeroesOfTheScript/includes/ready_function.js
+++ includes/assets/projects/HeroesOfTheScript/includes/ready_function.js
@@ -29,13 +29,13 @@ $(document).ready(function() {
     });
 
     // Automatic drops
-    // setInterval(function() {
-    //     var $el = $('main');
-    //     var x = Math.random() * $el.outerWidth();
-    //     var y = Math.random() * $el.outerHeight();
-    //     var dropRadius = 20;
-    //     var strength = 0.04 + Math.random() * 0.04;
+    setInterval(function() {
+        var $el = $('main');
+        var x = Math.random() * $el.outerWidth();
+        var y = Math.random() * $el.outerHeight();
+        var dropRadius = 20;
+        var strength = 0.04 + Math.random() * 0.04;
 
-    //     $el.ripples('drop', x, y, dropRadius, strength);
-    // }, 400);
+        $el.ripples('drop', x, y, dropRadius, strength);
+    }, 400);
 });
