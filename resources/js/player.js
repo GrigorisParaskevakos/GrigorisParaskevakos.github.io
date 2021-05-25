b45e22ed3b4ccd3db9abcf7faec175978c5d944f
GrigorisParaskevakos <paraskevakosg@gmail.com>
2021-05-25 19:59:15 +0300

    meta name=description and melody added

--- resources/js/player.js
+++ resources/js/player.js
@@ -32,20 +32,39 @@
  */
 class player {
     constructor() {
-        //let x = $("#muse"); 
+ 
         this.musicPlayer = {
-            play: function () {
-                document.getElementById("muse").play();
-                document.getElementById("muse").load();
-                document.getElementById("muse").volume = 1.000000;
-                setTimeout(
-                    function () {
-                        document.getElementById("muse").play();
-                    }, 1);
-            },
 
+            play: function (melody) {
+
+                switch (melody) {
+
+                    case "muse":
+                        document.getElementById("muse").play();
+                        document.getElementById("muse").load();
+                        document.getElementById("muse").volume = 1.000000;
+                        setTimeout(
+                            function () {
+                                document.getElementById("muse").play();
+                            }, 1);
+                        break;
+                    case "ocean":
+                        document.getElementById("ocean").play();
+                        document.getElementById("ocean").load();
+                        document.getElementById("ocean").volume = 1.000000;
+                        setTimeout(
+                            function () {
+                                document.getElementById("ocean").play();
+                            }, 1);
+                        break;
+                    default:
+                        console.log("No melody for playback...");
+                }
+
+            },
             pause: function () {
                 document.getElementById("muse").pause();
+                //todo
             }
         };
     }
